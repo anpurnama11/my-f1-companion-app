0526fd4f79146468a8447caaab4bddf591e352fc
anpurnama <anpurnama.27@gmail.com>
2026-07-25 14:28:05 +0700

    build: add androidx.glance:glance-appwidget and work-runtime-ktx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.glance.appwidget)
+    implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
